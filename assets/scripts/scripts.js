032c953bfc87ce0cba214ad217a111cabaeb5116
Sumayya Mussa <panjusumayya@gmail.com>
2024-08-06 18:37:59 +0000

    SP: Script is externally linked
    Internal script is removed from the page

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -22,3 +22,18 @@ document.addEventListener("DOMContentLoaded", function () {
         observer.observe(element);
     });
 });
+
+//Tab for schedules
+function openDay(evt, dayName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(dayName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
